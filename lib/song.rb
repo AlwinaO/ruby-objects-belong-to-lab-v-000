bb2d8bc4c5c926668af816af67fca113e1f3d117
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-03-15 02:11:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,15 +13,6 @@ class Song
   #
   # end
 
-  attr_accessor :title, :author
-
-  def author=(author)
-    author.class #=> Author
-    if author.is_a?(Author)
-    @author = author
-    else
-    raise TypeError, "Instance of Author expected, got #{author.class}"
-    end
-  end
+  attr_accessor :title
 
 end
